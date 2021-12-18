4446d9afacd5fcbe66d386e312bc1cc10c004c51
vizard24 <91430544+vizard24@users.noreply.github.com>
2021-12-17 22:27:14 -0500

    My tests
    TODO: 
    -Test sur les Controllers 
    -Test sur les Views

---
+++ test/TestView.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author yaovi gadedjro
+ *
+ */
+class TestView {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
